bb2ddab8fe11f6268fc047bc5b71ae3ff3956c49
dennis <dennis>
2008-07-02 11:52:30 +0000

    Erste technische Vorarbeiten zur logischen Modularisierung des viel zu lange gewordenen AHK-Skriptes.

---
+++ windows/autohotkey/Build-Update.bat
@@ -0,0 +1,32 @@
+@echo off
+
+
+
+echo Setting local path variables
+REM The path to the Auto Hotkeyprogram
+set ahk=C:\Programme\AutoHotkey
+REM The path to the authohotkey directory in the local svn copy
+set svn=.
+REM Just some usefull shortcuts:
+set scr="%svn%\Source"
+set fn=neo20-all-in-one
+
+
+
+echo Killing the old (AHK)Driver
+tskill neo20-all-in-one
+
+
+
+echo Creating a new Driver from the Source code
+copy /B "%scr%\Warning.ahk" + "%scr%\neo20-all-in-one.ahk" "%svn%\%fn%.ahk"
+
+
+echo Compiling the new Driver using Autohotkey
+"%ahk%\Compiler\Ahk2Exe.exe" /in "%svn%\%fn%.ahk" /out "%svn%\%fn%.exe" /icon "%svn%\neo.ico"
+
+
+echo Driver Update complete! You can now close this log-window.
+REM Start the new Driver
+%fn%.exe
+exit
