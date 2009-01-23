a99397a1ebd08d5d560c11b6490d1fc5e1961bd5
ben <ben>
2009-01-23 10:22:43 +0000

    autorun an das aktuelle asdf script angepasst

--- portable/autorun.sh
+++ portable/autorun.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-setxkbmap ie
-xmodmap neo/neo_de.xmodmap
+# CapsLock sollte aus sein
+xmodmap -e "clear Lock" 
+
+numlockx off
+# schalte autorepeat für modifier und deadkeys aus
+for key in 51 94 21 35 49; do
+	xset -r $key
+done
+setxkbmap lv
+xmodmap neo/neo_de.xmodmap || setxkbmap de
+
