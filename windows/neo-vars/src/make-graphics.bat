7eaba3ddff42c05f281678e85118cbcaf4864de4
dennis <dennis>
2009-02-02 14:53:47 +0000

    Ergänzung um ebene12.png (=CapsLock-Ebene) für die Bildschirmtastatur.

--- windows/neo-vars/src/make-graphics.bat
+++ windows/neo-vars/src/make-graphics.bat
@@ -1,2 +1,2 @@
 echo download the latest images for the screen keyboard
-for /L %%e in (1,1,6) do make-wget.exe -q -O ebene%%e.png http://neo-layout.org/grafik/tastatur3d/haupt_ziffern_feld/tastatur_neo_Ebene%%e.png
+for %%e in (1 12 2 3 4 5 6) do make-wget.exe -q -O ebene%%e.png http://neo-layout.org/grafik/tastatur3d/haupt_ziffern_feld/tastatur_neo_Ebene%%e.png
