f7693aee1f89055fbfd762c33c51aee279660a59
florian <florian@b9310e46-f624-0410-8ea1-cfbb3a30dc96>
2009-02-02 17:56:42 +0000

    Änderungen: Modifier-Änderung, Mod4Lock nun selektiv, kbdneo2.ahk schaltet nun auch den Treiber-Mod4Lock 1.) Modifier „neuverdrahtet“. Ermöglicht selektiven Mod4Lock und gleichzeitig stört damit die?\127?\127?\127?\127 aktuelle Logitech SetPoint-Version nicht mehr (Update für Logitech-Tastatur-Besitzer, die Setpoint nutzen dringend empfohlen!) 2.) aktueller Mod4Lock spart Funtktionstasten, tote Tasten und Numblock aus 3.) kbdneo2.ahk schaltet jetzt den Treiber-Mod4Lock
    Das Vöglein, das die Luft durchschifft,
    ist froh, dass es kein Schiffchen trifft.
    Denn träfe es ein Schiffchen,
    wüsst' es: Ich bin ein Fischchen.
    
    
    git-svn-id: https://svn.neo-layout.org@1628 b9310e46-f624-0410-8ea1-cfbb3a30dc96

--- windows/kbdneo2/Quelldateien/kbdneo2.h
+++ windows/kbdneo2/Quelldateien/kbdneo2.h
@@ -30,76 +30,74 @@
 \****+-------+_+----------+----------+----------+----------+----------+----------+*/
 // Tastenumbelegung für Neo 2.0 ; sonstige Tastenzuordnung siehe kbd.h
 #undef  T0D
-#define T0D _EQ(                           OEM_2                     )
+#define T0D _EQ(		 OEM_2		)
 #undef  T10
-#define T10 _EQ(                           'X'                       )
+#define T10 _EQ(		 'X'		)
 #undef  T11
-#define T11 _EQ(                           'V'                       )
+#define T11 _EQ(		 'V'		)
 #undef  T12
-#define T12 _EQ(                           'L'                       )
+#define T12 _EQ(		 'L'		)
 #undef  T13
-#define T13 _EQ(                           'C'                       )
+#define T13 _EQ(		 'C'		)
 #undef  T14
-#define T14 _EQ(                           'W'                       )
+#define T14 _EQ(		 'W'		)
 #undef  T15
-#define T15 _EQ(                           'K'                       )
+#define T15 _EQ(		 'K'		)
 #undef  T16
-#define T16 _EQ(                           'H'                       )
+#define T16 _EQ(		 'H'		)
 #undef  T17
-#define T17 _EQ(                           'G'                       )
+#define T17 _EQ(		 'G'		)
 #undef  T18
-#define T18 _EQ(                           'F'                       )
+#define T18 _EQ(		 'F'		)
 #undef  T19
-#define T19 _EQ(                           'Q'                       )
+#define T19 _EQ(		 'Q'		)
 #undef  T1A
-#define T1A _EQ(                           OEM_3                     )
+#define T1A _EQ(		 OEM_3		)
 #undef  T1B
-#define T1B _EQ(                           OEM_4                     )
+#define T1B _EQ(		 OEM_4		)
 #undef  T1E
-#define T1E _EQ(                           'U'                       )
+#define T1E _EQ(		 'U'		)
 #undef  T1F
-#define T1F _EQ(                           'I'                       )
+#define T1F _EQ(		 'I'		)
 #undef  T20
-#define T20 _EQ(                           'A'                       )
+#define T20 _EQ(		 'A'		)
 #undef  T21
-#define T21 _EQ(                           'E'                       )
+#define T21 _EQ(		 'E'		)
 #undef  T22
-#define T22 _EQ(                           'O'                       )
+#define T22 _EQ(		 'O'		)
 #undef  T23
-#define T23 _EQ(                           'S'                       )
+#define T23 _EQ(		 'S'		)
 #undef  T24
-#define T24 _EQ(                           'N'                       )
+#define T24 _EQ(		 'N'		)
 #undef  T25
-#define T25 _EQ(                           'R'                       )
+#define T25 _EQ(		 'R'		)
 #undef  T26
-#define T26 _EQ(                           'T'                       )
+#define T26 _EQ(		 'T'		)
 #undef  T27
-#define T27 _EQ(                           'D'                       )
+#define T27 _EQ(		 'D'		)
 #undef  T28
-#define T28 _EQ(                           'Y'                       )
+#define T28 _EQ(		 'Y'		)
 #undef  T29
-#define T29 _EQ(                           OEM_1                     )
+#define T29 _EQ(		 OEM_1		)
 #undef  T2B
-#define T2B _EQ(                           KANA                      ) // Mod 3
+#define T2B _EQ(		 OEM_102	)	// Mod 3
 #undef  T2C
-#define T2C _EQ(                           OEM_5                     )
+#define T2C _EQ(		 OEM_5		)
 #undef  T2D
-#define T2D _EQ(                           OEM_6                     )
+#define T2D _EQ(		 OEM_6		)
 #undef  T2E
-#define T2E _EQ(                           OEM_7                     )
+#define T2E _EQ(		 OEM_7		)
 #undef  T2F
-#define T2F _EQ(                           'P'                       )
+#define T2F _EQ(		 'P'		)
 #undef  T30
-#define T30 _EQ(                           'Z'                       )
+#define T30 _EQ(		 'Z'		)
 #undef  T31
-#define T31 _EQ(                           'B'                       )
+#define T31 _EQ(		 'B'		)
 #undef  T35
-#define T35 _EQ(                           'J'                       )
+#define T35 _EQ(		 'J'		)
 #undef  T3A
-#define T3A _EQ(                           KANA                      )// Mod 3
+#define T3A _EQ(		 OEM_102	)	// Mod 3
 #undef  X38
-//#define X38 _EQ(                           RMENU                     )// Mod 4
-#define X38 _EQ(                           OEM_102                     )
+#define X38 _EQ(		 OEM_8		)	// Mod 4
 #undef  T56
-//#define T56 _EQ(                           RMENU                     )// Mod 4
-#define T56 _EQ(                           OEM_102                     )
+#define T56 _EQ(		 OEM_8		)	// Mod 4
