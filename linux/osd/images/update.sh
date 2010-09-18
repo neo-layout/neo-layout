91286b0791146600ba5972c1bb094fc730c024ee
dennis <dennis@b9310e46-f624-0410-8ea1-cfbb3a30dc96>
2010-09-18 22:13:44 +0000

    Fix: Im Skript hatte sich noch ein Copy-Paste-Fehler eingeschlichen.
    git-svn-id: https://svn.neo-layout.org@2348 b9310e46-f624-0410-8ea1-cfbb3a30dc96

--- linux/osd/images/update.sh
+++ linux/osd/images/update.sh
@@ -11,11 +11,11 @@ wget -O neo2-hauptfeld_ebene5.png  http://neo-layout.org/svn/grafik/tastatur3d/h
 wget -O neo2-hauptfeld_ebene6.png  http://neo-layout.org/svn/grafik/tastatur3d/hauptfeld/tastatur_neo_Ebene6.png
 
 #Ziffernfeld:
-wget -O neo2-ziffernfeld_ebene1.png http://neo-layout.org/svn/grafik/tastatur3d/hauptfeld/tastatur_neo_Ebene1.png
-wget -O neo2-ziffernfeld_ebene1-caps.png http://neo-layout.org/svn/grafik/tastatur3d/hauptfeld/tastatur_neo_Ebene1Caps.png
-wget -O neo2-ziffernfeld_ebene2.png http://neo-layout.org/svn/grafik/tastatur3d/hauptfeld/tastatur_neo_Ebene2.png
-wget -O neo2-ziffernfeld_ebene2-caps.png http://neo-layout.org/svn/grafik/tastatur3d/hauptfeld/tastatur_neo_Ebene2Caps.png
-wget -O neo2-ziffernfeld_ebene3.png http://neo-layout.org/svn/grafik/tastatur3d/hauptfeld/tastatur_neo_Ebene3.png
-wget -O neo2-ziffernfeld_ebene4.png http://neo-layout.org/svn/grafik/tastatur3d/hauptfeld/tastatur_neo_Ebene4.png
-wget -O neo2-ziffernfeld_ebene5.png http://neo-layout.org/svn/grafik/tastatur3d/hauptfeld/tastatur_neo_Ebene5.png 
-wget -O neo2-ziffernfeld_ebene6.png http://neo-layout.org/svn/grafik/tastatur3d/hauptfeld/tastatur_neo_Ebene6.png
+wget -O neo2-ziffernfeld_ebene1.png http://neo-layout.org/svn/grafik/tastatur3d/ziffernfeld/tastatur_neo_Ebene1.png
+wget -O neo2-ziffernfeld_ebene1-caps.png http://neo-layout.org/svn/grafik/tastatur3d/ziffernfeld/tastatur_neo_Ebene1Caps.png
+wget -O neo2-ziffernfeld_ebene2.png http://neo-layout.org/svn/grafik/tastatur3d/ziffernfeld/tastatur_neo_Ebene2.png
+wget -O neo2-ziffernfeld_ebene2-caps.png http://neo-layout.org/svn/grafik/tastatur3d/ziffernfeld/tastatur_neo_Ebene2Caps.png
+wget -O neo2-ziffernfeld_ebene3.png http://neo-layout.org/svn/grafik/tastatur3d/ziffernfeld/tastatur_neo_Ebene3.png
+wget -O neo2-ziffernfeld_ebene4.png http://neo-layout.org/svn/grafik/tastatur3d/ziffernfeld/tastatur_neo_Ebene4.png
+wget -O neo2-ziffernfeld_ebene5.png http://neo-layout.org/svn/grafik/tastatur3d/ziffernfeld/tastatur_neo_Ebene5.png
+wget -O neo2-ziffernfeld_ebene6.png http://neo-layout.org/svn/grafik/tastatur3d/ziffernfeld/tastatur_neo_Ebene6.png
