38bb977b2086959d28919666c86a4a4c23841fb9
Philip Zander <philip.zander@gmail.com>
2020-10-02 12:55:02 +0200

    Add WOW64 configuration to manual build scripts

---
+++ windows/kbdneo2/build/build_variant_x86_wow64.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+if "%1"=="" (
+	ECHO Dieses Skript ist nur zur internen Verwendung von build_x86 gedacht.
+	EXIT /B
+)
+
+CL /c /IRelease\ /I..\..\inc /Zi /nologo /W4 /WX /diagnostics:column /O2 /Oy- /D _X86_=1 /D i386=1 /D BUILD_WOW6432 /D STD_CALL /D WIN32_LEAN_AND_MEAN=1 /D _WIN32_WINNT=0x0A00 /D WINVER=0x0A00 /D WINNT=1 /D NTDDI_VERSION=0x0A000007 /D _WINDLL /Gm- /MD /GS /guard:cf /fp:precise /Zc:wchar_t- /Zc:forScope /Zc:inline /Fo"x86-wow64\\" /Fd"x86-wow64\vc142.pdb" /Gz /TC /analyze- /FC /errorReport:prompt kbd%1.c
+
+RC /D _X86_=1 /D i386=1 /D STD_CALL /D WIN32_LEAN_AND_MEAN=1 /D _WIN32_WINNT=0x0A00 /D WINVER=0x0A00 /D WINNT=1 /D NTDDI_VERSION=0x0A000007 /l"0x0409" /Ix86\ /I..\..\inc /nologo /fo"x86-wow64\kbd%1.res" kbd%1.rc
+
+LINK /ERRORREPORT:PROMPT /OUT:"x86-wow64\kbd%1.dll" /INCREMENTAL:NO /NOLOGO /WX kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /DEF:"kbd%1.def" /MANIFEST /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /manifest:embed /DEBUG /PDB:"x86-wow64\kbd%1.pdb" /TLBID:1 /NOENTRY /DYNAMICBASE /NXCOMPAT /IMPLIB:"x86-wow64\kbd%1.lib" /MACHINE:X86 /SAFESEH /guard:cf /IGNORE:4198,4010,4037,4039,4065,4070,4078,4087,4089,4221  -merge:.edata=.data -merge:.rdata=.data -merge:.text=.data -merge:.bss=.data -section:.data,re  /ignore:4254 /DLL x86-wow64\kbd%1.res x86-wow64\kbd%1.obj
--- windows/kbdneo2/build/build_x86.cmd
+++ windows/kbdneo2/build/build_x86.cmd
@@ -6,17 +6,23 @@ REM Build Bone, Neo, Qwertz
 
 cd kbdbone
 IF NOT EXIST x86 mkdir x86
+IF NOT EXIST x86-wow64 mkdir x86-wow64
 call ..\..\build\build_variant_x86.cmd bone
+call ..\..\build\build_variant_x86_wow64.cmd bone
 cd ..
 
 cd kbdneo2
 IF NOT EXIST x86 mkdir x86
+IF NOT EXIST x86-wow64 mkdir x86-wow64
 call ..\..\build\build_variant_x86.cmd neo2
+call ..\..\build\build_variant_x86_wow64.cmd neo2
 cd ..
 
 cd kbdgr2
 IF NOT EXIST x86 mkdir x86
+IF NOT EXIST x86-wow64 mkdir x86-wow64
 call ..\..\build\build_variant_x86.cmd gr2
+call ..\..\build\build_variant_x86_wow64.cmd gr2
 cd ..
 
 popd
