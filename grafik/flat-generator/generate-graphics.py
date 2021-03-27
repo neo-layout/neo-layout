0bcd1b85a1ad49c08806aa26b772285d3cac4618
Ulli Kehrle <ulli.kehrle@rwth-aachen.de>
2021-03-27 06:25:00 +0100

    Grafiken verbessert
    Es werden folgenden Probleme behoben:
     - auf der zweiten Ebene ist statt der Tilde eine zweite Cedille.
     - Die Leertaste ist nicht beschriftet
    
    Weiter wurden generierte Dateien entfernt, die verschiedenen Templates
    zusammengefasst und das Erzeugungsshellskript durch ein Makefile
    ersetzt.
    
    Closes neo/dokumentation#18.

--- grafik/flat-generator/generate-graphics.py
+++ grafik/flat-generator/generate-graphics.py
@@ -7,13 +7,15 @@ import sys
 import replacements
 
 if len(sys.argv) == 1:
-    print('Usage: ./<this script> "layoutname" template')
+    print('Usage: ./<this script> variantname [numpad]')
     exit(1)
 
 layout = sys.argv[1]
-templatename = sys.argv[2]
+templatename = "base.svg.template"
+numpad = True if len(sys.argv) == 3 and sys.argv[2] == "numpad" else False
+swap_m3r_ä = True if layout == "de vou" else False
 
-os.system("setxkbmap " + layout + " -print | xkbcomp -xkb - /tmp/keymaptmp 2>/dev/null")
+os.system("setxkbmap de " + layout + " -print | xkbcomp -xkb - /tmp/keymaptmp 2>/dev/null")
 # TODO: actually write/generate a proper parser for xkbmaps
 os.system(r'''sed -n '/xkb_symbols/,/xkb_geometry/p' /tmp/keymaptmp | tail -n +2 | grep -e 'key' -e symbols -e '}' | sed 's/symbols\[Group1]=//' | paste -sd "" - | sed 's/\;/&\n/g' | grep -v 'modifier_map' | sed -r 's/\s//g' | sed -r 's/key<(.*)>\{\[/\1=/g' | sed -r 's/\]?,?\}\;//' | grep -v '^$' > /tmp/keymap''')
 
@@ -42,14 +44,16 @@ with open('/tmp/keymap', 'r') as file:
   # replace keynames with the symbol they produce
   keymap = {a: list(map(replacements.f, b)) for a,b in keymap.items()}
 
-
   for layer in range(0,7): # 7 because the last layer is empty
       # create a dict with the replacements from repalcements.py
       layerdict = {a: b[layer] for a,b in keymap.items()}
       # color modifiers accordingly
       for x in modifiers[layer]:
           layerdict[x] = " pressed"
-      out = open(layout + " ebene " + layernames[layer] + ".svg", "w")
+      layerdict["numpad"] = numpad
+      layerdict["swap_m3r_ä"] = swap_m3r_ä
+      versionstring = "-numpad" if numpad else "-tkl"
+      out = open(layout + "-Ebene-" + layernames[layer] + versionstring + ".svg", "w")
       with open(templatename) as templatefile:
           template = Template(templatefile.read())
       out.write(template.render(layerdict))
